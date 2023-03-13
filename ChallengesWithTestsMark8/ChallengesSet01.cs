7dcce749d95e837446f1129a3c7cd8cebca5fc85
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-13 14:46:22 -0600

    Completed 1st section

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 return true;
             }
@@ -18,32 +18,51 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 <= number2)
+            {
+                return number1;
+            }
+            else 
+            {
+                return number2;
+            }
+            
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            string name = nameOfPerson;
+            if (string.IsNullOrEmpty(name))
+            {
+               return "Hello!";
+            }
+            else
+            return ($"Hello, {name}!");
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            string name = Console.ReadLine();
+            Console.WriteLine("Hey!,{name}");
+            return name;
+              
+
+
         }
     }
 }
